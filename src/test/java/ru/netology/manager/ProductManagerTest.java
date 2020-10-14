53c8d130e098c73cab870c3153e8f9fc63ffd635
Фадеева Дарья Сергеевна <ds.fadeeva@expoforum.ru>
2020-10-14 17:18:44 +0300

    Added ProductManager

---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,82 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+    private ProductRepository repository = new ProductRepository();
+    private ProductManager productManager = new ProductManager(repository);
+    private Book first = new Book(1, "name1", 10, "author1");
+    private Book second = new Book(2, "name2", 20, "author2");
+    private Smartphone third = new Smartphone(3, "name3", 30, "vendor1");
+    private Smartphone forth = new Smartphone(4, "name4", 40, "vendor2");
+
+    @BeforeEach
+    public void setUp() {
+        productManager.add(first);
+        productManager.add(second);
+        productManager.add(third);
+        productManager.add(forth);
+    }
+
+    @Test
+    public void shouldSearchBookName1() {
+        String text = "name1";
+
+        Product[] expected = new Product[]{first};
+        Product[] actual = productManager.searchBy(text);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchBookName2() {
+        String text = "name2";
+
+        Product[] expected = new Product[]{second};
+        Product[] actual = productManager.searchBy(text);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchBookAuthor1() {
+        String text = "author1";
+
+        Product[] expected = new Product[]{first};
+        Product[] actual = productManager.searchBy(text);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchBookAuthor2() {
+        String text = "author2";
+
+        Product[] expected = new Product[]{second};
+        Product[] actual = productManager.searchBy(text);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchSmartphoneName3() {
+        String text = "name3";
+
+        Product[] expected = new Product[]{third};
+        Product[] actual = productManager.searchBy(text);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchSmartphoneVendor4() {
+        String text = "vendor2";
+
+        Product[] expected = new Product[]{forth};
+        Product[] actual = productManager.searchBy(text);
+        assertArrayEquals(expected, actual);
+    }
+
+}
